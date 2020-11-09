86ee0096ff6c8e7ef77d1d99a280b8a69f268a10
José Atienza Piedra <joseatienzapiedra@gmail.com>
2020-11-09 09:48:07 +0100

    Optimized code for BT COM

--- ArduinoCode/ArduinoCode.ino
+++ ArduinoCode/ArduinoCode.ino
@@ -1,99 +1,78 @@
-/* PROGRAMACIÓN ROBÓTICA 1º MEL 
- */
-//DECLARAMOS LOS PINES DE SALIDA PARA LOS 3 LM323n
-/*PRIMER LM323n
- * 
- */
-//MOTOR 1
-int M11 = 2;
-int M12 = 3;
-//MOTOR 2
-int M21 = 4;
-int M22 = 5;
-//MOTOR 3
-int M31 = 6;
-int M32 = 7;
-//MOTOR 4
-int M41 = 8;
-int M42 = 9;
-//MOTOR 5
-int M51 = 10;
-int M52 = 11;
-//MOTOR 6
-int M61 = 12;
-int M62 = 13;
+//ROBOT MENTOR - 1º MEL IES El ARGAR
 
-int pwmvel=100;
-int serialin;
+#include <SoftwareSerial.h> 
 
-void setup() {
+//Definición de los pines de control de motores (De momento no se utiliza) 
+/*
+#define M11 2
+#define M12 3
+#define M21 4
+#define M22 5
+#define M31 6
+#define M32 7
+#define M41 8
+#define M42 9
+#define M51 10
+#define M52 11
+#define M61 12
+#define M62 13*/
 
-  Serial.begin(9600);
-//DECLARAMOS LOS PINES COMO SALIDA
-  pinMode (M11, OUTPUT);
-  pinMode (M12, OUTPUT);
-  pinMode (M21, OUTPUT);
-  pinMode (M22, OUTPUT);
-  pinMode (M31, OUTPUT);
-  pinMode (M32, OUTPUT);
-  pinMode (M41, OUTPUT);
-  pinMode (M42, OUTPUT);
-  pinMode (M51, OUTPUT);
-  pinMode (M52, OUTPUT);
-  pinMode (M61, OUTPUT);
-  pinMode (M62, OUTPUT);
-}
+//Definimos pines y puerto serial software para comunicaciones BT
+#define rxPin 50
+#define txPin 52
+SoftwareSerial BTSerial =  SoftwareSerial(rxPin, txPin);
 
-void loop() 
+int pwmvel = 0, serialin; //Declaramos la velocidad PWM a 0, y la variable en la que se almacenan los datos recibidos.
+
+void setup() 
 {
-  if (Serial.available())
-  {
-    serialin=Serial.parseInt();
-    switch (serialin)
-    {
-      case 100:
-      pwmvel = 100;
-      break;
-      
-      case 150:
-      pwmvel = 150;
-      break;
-      
-      case 255:
-      pwmvel = 255;
-      break;
-
-      case 15:
-      StopMotor();
-      break;
-      
-      default:
-      MovMotor(serialin);
-      break;
-      
-      }
-    }
+  BTSerial.begin(9600);   //Arrancamos puerto serial para comunicación BlueTooth
+  pinsOutput();           //Llamada a la función de declaración de puertos PWM como salida.
+  pinMode(rxPin, INPUT);  //Establecemos pin de entrada BT como entrada
+  pinMode(txPin, OUTPUT); //Establecemos pin de salida BT como salida
 }
 
-//DECLARAMOS LAS FUNCIONES QUE VAMOS A UTILIZAR
-//MOTOR 1
-void MovMotor (int ACT)
+void loop()
+{
+  if (BTSerial.available())                                   //Si hay datos en el puerto BT...
+  {
+    serialin = BTSerial.parseInt();                           //...Los parseamos a tipo int.
+
+    if (serialin > 0 && serialin < 256)                       //Si el valor está entre 1 y 255...
+    {
+      pwmvel = serialin;                                      //...Se considera valor pwm y se asigna como velocidad.
+    }
+    else if (serialin == 0)                                   //Si por el contrario el valor es 0...
+    {
+      StopMotor();                                            //Se paran los motores.
+    }
+    else                                                      //Si no...
+    {
+      MovMotor (map(serialin, 256, 267, 2, 13));              //Se mapea el dato recibido, haciendo que el 256 corresponda con el valor 2 y el 267 con el valor 13.
+    }                                                         //Y se invoca la función que permite mover el motor correspondiente.
+  }
+}
+
+
+void MovMotor (int ACT)                                     //Cuando se invoca, mueve el motor correspondiente a la velocidad seleccionada.
 {
   analogWrite (ACT, pwmvel);
 }
 
-void StopMotor()
+void StopMotor()                                            //Cuando se invoca para todos los motores, poniendo el pwm a 0.
 {
-  digitalWrite (M11, LOW);
-  digitalWrite (M12, LOW);
-  digitalWrite (M21, LOW);
-  digitalWrite (M22, LOW);
-  digitalWrite (M31, LOW);
-  digitalWrite (M32, LOW);
-  digitalWrite (M41, LOW);
-  digitalWrite (M42, LOW);
-  digitalWrite (M51, LOW);
-  digitalWrite (M52, LOW);
-  digitalWrite (M61, LOW);
-  digitalWrite (M62, LOW);
+  int i;
+  for (i = 2; i < 14; i++)
+  {
+    analogWrite(i, 0);
+  }
+}
+
+void pinsOutput()
+{
+  int i;
+  for (i = 2; i < 14; i++)
+  {
+    pinMode (i, OUTPUT);
+  }
 }
